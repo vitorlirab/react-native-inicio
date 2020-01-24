87b18ed33908729b153efd037ef1ac60750546b6
João Vitor Lira Bezerra <vitor.lira98@gmail.com>
2020-01-24 19:02:28 -0300

    Remover produto

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,12 +4,20 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
       return produce(state, draft => {
-        const producIndex = draft.findIndex(p => p.id === action.product.id);
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
 
-        if (producIndex >= 0) {
-          draft[producIndex].amount += 1;
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
         } else {
           draft.push({ ...action.product, amount: 1 });
+        }
+      });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
         }
       });
     default:
